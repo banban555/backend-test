d3cc631e27243b12dfcbf14abf76d27e58187173
yunhyungNAM <iys04003@naver.com>
2023-05-03 16:25:40 +0900

    FEAT: 서버에 몽고DB 연동

--- Project/server.js
+++ Project/server.js
@@ -1,33 +1,34 @@
-var express = require("express");
-var app = express();
-// const MongoClient = require('mongodb').MongoClient;
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
 
-// var db;
-// MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.2jx3ncv.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true }, function (에러, client) {
-// 	if (에러) return console.log(에러)
-// 	db = client.db('serverprac');
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(8080, function () {
-  console.log("listening on 8080");
-});
-// });
+var db;
+MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.2jx3ncv.mongodb.net/?retryWrites=true&w=majority', function(에러, client){
+  if (에러) return console.log(에러)
+  db = client.db('serverprac');
 
-// app.post('/add', function(요청, 응답){
-//   응답.send('전송완료');
-//   console.log(요청.body.FirstName);
-//   console.log(요청.body.LastName);
-//   console.log(요청.body.EmailAddress);
-//   console.log(요청.body.Password);
-//   db.collection('post').insertOne({이름: 요청.body.FirstName, 성: 요청.body.LastName, 이메일주소: 요청.body.EmailAddress, 비밀번호: 요청.body.Password},function(){
-//     console.log("저장완료");
-//   })
-// })
+  app.listen(8080, function() {
+    console.log('listening on 8080');
+  })
+})
+
+
+
+
+app.post('/signup', function(요청, 응답){
+  응답.send('전송완료');
+  console.log(요청.body.FirstName);
+  console.log(요청.body.LastName);
+  console.log(요청.body.EmailAddress);
+  console.log(요청.body.Password);
+  db.collection('post').insertOne({이름: 요청.body.FirstName, 성: 요청.body.LastName, 이메일주소: 요청.body.EmailAddress, 비밀번호: 요청.body.Password},function(){
+    console.log("저장완료");
+  })
+})
 
 app.use(express.static(__dirname + "/react-web/build/"));
-
-app.get("/", (요청, 응답) => {
-  응답.sendFile(__dirname + "/react-web/build/index.html");
-});
 
 app.get("*", function (요청, 응답) {
   응답.sendFile(__dirname + "/react-web/build/index.html");

f4a49c044d6610d37aa78ef94d7fb56fc40bffa4
banban555 <ban_ban_55@naver.com>
2023-05-25 19:26:04 +0900

    modal component 생성

--- Project/react-web/src/pages/SignIn.js
+++ Project/react-web/src/pages/SignIn.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import theme from "../styles/theme";
+
+import Modal from "../components/common/Modal";
 import Button from "../components/common/Button";
 import TextComponent from "../components/common/TextComponent";
 
 const SignIn = () => {
-  const [errorModalVisible, setErrorModalVisible] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
@@ -23,8 +24,7 @@ const SignIn = () => {
       .then((res) => {
         console.log(res.data);
         if (res.data.loginSuccess === false) {
-          setErrorModalVisible(true);
-          setErrorMessage(res.data.message);
+          setModalVisible(true);
         }
         if (res.data.loginSuccess === true) {
           console.log("로그인 성공");
@@ -36,9 +36,8 @@ const SignIn = () => {
       });
   };
 
-  const handleCloseErrorModal = () => {
-    setErrorModalVisible(false);
-    setErrorMessage("");
+  const handleCloseModal = () => {
+    setModalVisible(false);
   };
 
   return (
@@ -56,12 +55,11 @@ const SignIn = () => {
         />
       </Form>
 
-      <ModalContainer open={errorModalVisible}>
-        <ModalContent>
-          <CloseButton onClick={handleCloseErrorModal}>&times;</CloseButton>
-          <p>{errorMessage}</p>
-        </ModalContent>
-      </ModalContainer>
+      <Modal
+        isOpen={modalVisible}
+        handleClose={handleCloseModal}
+        errorMessage="아이디와 비밀번호를 확인해주세요"
+      />
     </SignInContainer>
   );
 };
@@ -94,40 +92,6 @@ const Input = styled.input`
 `;
 
 const PasswordInput = styled(Input).attrs({ type: "password" })``;
-
-const ModalContainer = styled.div`
-  display: ${(props) => (props.open ? "block" : "none")};
-  position: fixed;
-  z-index: 1000;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  overflow: auto;
-  background-color: rgba(0, 0, 0, 0.4);
-`;
-
-const ModalContent = styled.div`
-  background-color: #fefefe;
-  margin: 15% auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 30%;
-`;
-
-const CloseButton = styled.span`
-  color: #aaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-
-  &:hover,
-  &:focus {
-    color: black;
-    text-decoration: none;
-    cursor: pointer;
-  }
-`;
 
 const StyledImage = styled.img`
   width: 32rem;

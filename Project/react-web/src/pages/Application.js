38c06e9c844cf41a8aba502edf908c69fd423695
lys3269 <lys3269@dgu.ac.kr>
2023-06-09 08:02:48 +0900

    feat:버튼이름변경 마이페이지 -> 회원정보수정 & 회원정보 수정시 모달추가

--- Project/react-web/src/pages/Application.js
+++ Project/react-web/src/pages/Application.js
@@ -330,7 +330,7 @@ const Application = () => {
                   window.location.href = "/mypage";
                 }}
               >
-                마이페이지
+                회원정보수정
               </Button>
             </div>
           </div>
